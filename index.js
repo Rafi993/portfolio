0da5384b89da17aa56a071ae5123df5b09a1bf2e
mohamedrafi <fair7993@gmail.com>
2018-04-19 12:54:25 +0530

    bot with hard-coded response added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let currentColor = 0;
+let pressCounter = 0;
+/**
+ * This functions change the color of elements on page by adding class to body
+ */
 const changeColor = ()=>{
   let newBackground = 0;
+  pressCounter ++;
   if(currentColor<4) {
     currentColor++;
     newBackground =`color-${currentColor}`;
@@ -8,8 +13,67 @@ const changeColor = ()=>{
     currentColor=0;
     newBackground =`color-${currentColor}`;
   }
+
+  if(pressCounter > 10){
+    reply('', 'Settle on a color will you!!!')
+  }
   
   const body = document.getElementsByTagName('body')[0];
   // Replace class
   body.className = body.className.replace(/\bcolor-[0-9]\b/g , '' ) + newBackground;
 }
+
+/**
+ * Reply to user
+ */
+const reply = (req='', res='')=>{
+  let speechBubble = document.getElementById('response');
+  let response = ''
+  if(res!=='') {
+    response = res;
+   }
+   speechBubble.innerHTML = response;
+   speechBubble.style.visibility = 'visible';
+}
+
+const defaultConversation = [
+  'Hi there',
+  'I\'m <b>Rafi</b>',
+  'I\'m Developer',
+  'Who works mostly in <b>JavaScript</b> and <b>Python</b>',
+  'In my free time I contribute to <b>Open source</b> and talk tech',
+  'Actually I just realized I\'m just a bot',
+  'Ask me whatever you want human']
+
+const standardResponse = {
+  'open github': ()=> { window.open('https://github.com/Rafi993', '_blank').focus() },
+  'open stackoverflow': ()=> { window.open('https://stackoverflow.com/users/2445295/user93', '_blank').focus() }, 
+  'open blog': ()=> {window.open('https://medium.com/@notsohuman','_blank').focus()},
+  'mail me': ()=> {window.open('https://medium.com/@notsohuman','_blank').focus()}
+}
+
+const talk = i=>{
+  if(defaultConversation[i]!== undefined) {
+    setTimeout(()=> {
+      reply('', defaultConversation[i])
+      talk(i+1)
+    }, 2000)
+  }
+}
+
+talk(0);
+
+
+//  TODO: Remove hard-coded response and add bot api
+document.getElementById("talk")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+      if(standardResponse[event.target.value] !== undefined) {
+        standardResponse[event.target.value]();
+      }
+    }
+});
+
+
+console.log('There is nothing here the code for this repo is at https://github.com/Rafi993/portfolio')
